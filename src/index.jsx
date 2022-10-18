5f64e5c655f02f898f7a722ee2a40f5212dde5b2
Pranav <pranav.techiegeek@gmail.com>
2022-10-18 18:18:01 +0530

    createRoot + favicon

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import App from "./App";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOMClient.createRoot(document.getElementById("root")).render(<App />);
 
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://www.snowpack.dev/#hot-module-replacement
 if (import.meta.hot) {
-	import.meta.hot.accept()
+  import.meta.hot.accept();
 }
