40256cd1f6c7334f8e9fb308dcbc7b1f93512b1b
ZinoTrust <zinotrust@gmail.com>
2023-06-15 18:33:40 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import React from "react";
-import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="/logo.svg" className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload!
-        </p>
-        <span className="App-link">Hello from codedamn :)</span>
-      </header>
+    <div>
+      <h1>Hello World!!!</h1>
     </div>
   );
-}
+};
 
 export default App;
